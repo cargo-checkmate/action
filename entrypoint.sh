329bcf94715333b86375583e463fbe1afe0ef429
Nate Wilcox <nathan+dev@electriccoin.co>
2023-04-15 09:30:07 -0700

    Add a bunch of diagnostics.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 set -x
+
+pwd
+git status
+git rev-parse --git-dir
+git rev-parse --show-toplevel
+git log --pretty='%Cred%h%Creset %C(yellow)%ai%Creset - %s <%C(bold blue)%ae%Creset>%Cgreen%d%Creset' --graph -10
+
 exec cargo checkmate "$1"
